a93af325d3a186a5801c5493d53e0be53f493b92
peterkuzmenko1986 <peterkuzmenko1986@gmail.com>
2023-04-20 23:35:49 +0200

    calc adding btnsEvents

--- Calculator/scripts/script.js
+++ Calculator/scripts/script.js
@@ -7,7 +7,7 @@ viewer.classList.add("viewer");
 calcBody.append(viewer);
 
 let memConteiner = document.createElement("div");
-memConteiner.classList.add("memConteiner");
+memConteiner.classList.add("btnsLine");
 calcBody.append(memConteiner);
 
 let memBtn = document.createElement("button");
@@ -16,13 +16,11 @@ memBtn.innerText = "MEM";
 memConteiner.append(memBtn);
 
 let memMin = document.createElement("button");
-// memMin.classList.add("memMin");
 memMin.classList.add("memBtn");
 memMin.innerText = "- P";
 memConteiner.append(memMin);
 
 let memPls = document.createElement("button");
-// memPls.classList.add("memPls");
 memPls.classList.add("memBtn");
 memPls.innerText = "+ P";
 memConteiner.append(memPls);
@@ -35,7 +33,7 @@ memConteiner.append(resetBtn);
 
 
 let line789 = document.createElement("div");
-line789.classList.add("memConteiner");
+line789.classList.add("btnsLine");
 calcBody.append(line789);
 
 let btn7 = document.createElement("button");
@@ -62,7 +60,7 @@ line789.append(btnDivide);
 
 
 let line456 = document.createElement("div");
-line456.classList.add("memConteiner");
+line456.classList.add("btnsLine");
 calcBody.append(line456);
 
 let btn4 = document.createElement("button");
@@ -88,7 +86,7 @@ line456.append(btnMult);
 
 
 let line123 = document.createElement("div");
-line123.classList.add("memConteiner");
+line123.classList.add("btnsLine");
 calcBody.append(line123);
 
 let btn1 = document.createElement("button");
@@ -115,7 +113,7 @@ line123.append(btnMin);
 
 
 let line0 = document.createElement("div");
-line0.classList.add("memConteiner");
+line0.classList.add("btnsLine");
 calcBody.append(line0);
 
 let btn0 = document.createElement("button");
@@ -138,3 +136,121 @@ btnIs.classList.add("memBtn");
 btnIs.classList.add("resetBtn");
 btnIs.innerText = "=";
 line0.append(btnIs);
+
+
+//select Buff1 --- true
+//select Buff2 --- false
+let buffSelect = true;
+
+let buff1 = 0, buff2 = 0, operation;
+let memBuff, memBuffPls, memBuffMin;
+
+let viewerBuff = 0;
+viewer.innerText = viewerBuff;
+
+
+calcBody.addEventListener("click", funcBtnClk, false);
+
+function funcIs(){
+	switch(operation){
+	case "mult":
+		viewerBuff = buff1 * buff2;
+		break;
+	case "divide":
+		viewerBuff = buff1 / buff2;
+		break;
+	case "plus":
+		viewerBuff = buff1 + buff2;
+		break;
+	case "minus":
+		viewerBuff = buff1 - buff2;
+	}
+}
+
+function addToBuff(param){
+	if(buffSelect == true)
+		{
+			buff1 += param.toString();
+			buff1 = parseFloat(buff1);
+			viewerBuff = buff1;
+		}
+	else
+		{
+			buff2 += param.toString();
+			buff2 = parseFloat(buff2);
+			viewerBuff = buff2;
+		}
+}
+
+function funcBtnClk(e){
+	switch(e.target){
+	case btnIs:
+		funcIs();
+		break;
+
+	case btnMult:
+		operation = "mult";
+		buffSelect = false;
+		break;
+	case btnDivide:
+		operation = "divide";
+		buffSelect = false;
+		break;
+	case btnPlus:
+		operation = "plus";
+		buffSelect = false;
+		break;
+	case btnMin:
+		operation = "minus";
+		buffSelect = false;
+		break;
+
+
+	case memBtn:
+		break;
+	case memMin:
+		break;
+	case memPls:
+		break;
+	case resetBtn:
+		break;
+
+
+	case btnPoint:
+		if(operation !="complite") 
+		break;
+
+	case btn0:
+		addToBuff(0);
+		break;
+	case btn1:
+		addToBuff(1);
+		break;
+	case btn2:
+		addToBuff(2);
+		break;
+	case btn3:
+		addToBuff(3);
+		break;
+	case btn4:
+		addToBuff(4);
+		break;
+	case btn5:
+		addToBuff(5);
+		break;
+	case btn6:
+		addToBuff(6);
+		break;
+	case btn7:
+		addToBuff(7);
+		break;
+	case btn8:
+		addToBuff(8);
+		break;
+	case btn9:
+		addToBuff(9);
+		break;
+	}
+
+	viewer.innerText = viewerBuff;
+}
