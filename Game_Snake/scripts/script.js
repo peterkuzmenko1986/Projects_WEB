e6a020f4948fa0e0ba789963df79dc13546ad85f
peterkuzmenko1986 <peterkuzmenko1986@gmail.com>
2023-05-08 21:03:59 +0200

    Game Snake adding interactive

--- Game_Snake/scripts/script.js
+++ Game_Snake/scripts/script.js
@@ -13,21 +13,55 @@ document.styleSheets[1].insertRule(`.mainDiv{padding: ${padding}rem; }`);
 document.body.prepend(mainDiv);
 
 
-let elems = new Array(n);
+let elem = new Array(n);
 let line;
 for(let i=0; i<n; i++){
-    elems[i] = new Array(n);
+    elem[i] = new Array(n);
     line = document.createElement("div");
     line.classList.add("line");
     mainDiv.append(line);
 
     for(let j=0;j<n; j++){
-        elems[i][j] = document.createElement("div");
-        elems[i][j].classList.add("pixel");
-        line.append(elems[i][j]);
+        elem[i][j] = document.createElement("div");
+        elem[i][j].classList.add("pixel");
+        line.append(elem[i][j]);
     }
 }
 
 let calcWidth = width/n - 2*padding + "rem";
 document.styleSheets[1].insertRule(`.pixel{width:${calcWidth}; height: ${calcWidth}}`);
 
+setInterval(funcInterval, 1000);
+
+let isActive = false;
+let isPosChanged = false;
+
+let posX = Math.floor( (n-1)/2);
+let posY = n-1;
+function funcInterval(){
+    isPosChanged = false;
+    isActive = !isActive;
+    elem[posY][posX].classList.toggle("grey");
+}
+
+document.addEventListener("keydown", funcKey, false);
+
+function funcKey(e){
+    if(!isActive && !isPosChanged){
+        switch(e.code){
+            case "ArrowUp":
+                if(posY>0) posY--;
+            break;
+            case "ArrowDown":
+                if(posY<(n-1)) posY++;
+            break;
+            case "ArrowLeft":
+                if(posX>0) posX--;
+            break;
+            case "ArrowRight":
+                if(posX<(n-1)) posX++;
+            break;
+        }
+        isPosChanged = true;
+    }
+}
